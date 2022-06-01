03b715e49a8f694a2aaa3ffd8b9ac5052cd07220
Yuri Solyanik <jura.s.n@gmail.com>
2022-06-01 11:04:47 +0500

    Hash complete

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,31 @@
+import { access, readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import crypto from 'crypto';
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const folderName = 'files';
+  const fileName = 'fileToCalculateHashFor.txt';
+  const filePath = join(__dirname, folderName, fileName);
+
+  const hashSum = crypto.createHash('sha256');
+
+  access(filePath)
+    .then(() => {
+      readFile(filePath)
+        .then((fileData) => {
+          hashSum.update(fileData);
+          const hex = hashSum.digest('hex');
+          console.log(hex);
+        })
+    })
+    .catch(() => {
+      console.log(new Error('FS operation failed'));
+    })
+
 };
+
+calculateHash();
