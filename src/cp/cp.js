00fabdbf83a2d50de9abdd63760cd5ee604eb8da
Yuri Solyanik <jura.s.n@gmail.com>
2022-06-03 08:34:21 +0500

    Child Processes: Refactoring

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -11,16 +11,25 @@ const filePath = join(__dirname, folderName, fileName);
 
 export const spawnChildProcess = async (args) => {
   const child = fork(filePath, args);
+
   console.log(`Fork child process with PID: ${child.pid}`);
-  process.stdin.on('data', (data) => child.stdin.send(data));
+
+  process.stdin.on('data', (data) => {
+    if (child) {
+      child.send(data);
+    }
+  });
+
   child.on('message', (msg) => {
     console.log(`Message from child process: \n${msg}`);
   });
+
   child.on('exit', function (code, signal) {
     console.log(`Child process exited with code ${code} and signal ${signal}`);
     process.exit();
   });
 };
 
+// spawnChildProcess([ '123', '321', '000', 'f' ]);
 // spawnChildProcess([ 'ls', '-lha' ]);
 spawnChildProcess([ 'find', './src/fs', '-type', 'f' ]);
--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -4,6 +4,7 @@ const args = process.argv.slice(2);
 
 console.log(`Total number of arguments is ${args.length}`);
 console.log(`Arguments: ${JSON.stringify(args)}`);
+console.log('---------------------------');
 
 const echoInput = (chunk) => {
   const chunkStringified = chunk.toString();
